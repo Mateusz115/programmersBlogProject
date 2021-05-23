39702ae28335aa32e6c40726c4bca13aed0a8280
Mateusz <matjas1501@wp.pl>
2021-05-23 20:25:06 +0200

    ordered posts on the home page

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-createDate')
     return render(request, 'home.html',{'posts': posts})
 
 def detail(request, postId):
